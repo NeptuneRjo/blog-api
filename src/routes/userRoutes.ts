7e5cf584b1c7bd13a85637dbb8652c9e96a6743a
NeptuneRjo <rjo9000@outlook.com>
2022-08-05 18:10:11 -0400

    add get route and get controller

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
-import { post_user, delete_user } from '../controllers/userControllers'
+import {
+	post_user,
+	delete_user,
+	get_user,
+} from '../controllers/userControllers'
 const router = Router()
 
 router.route('/').post(post_user)
-router.route('/:id').delete(delete_user)
+router.route('/:id').get(get_user).delete(delete_user)
 
 export default router
