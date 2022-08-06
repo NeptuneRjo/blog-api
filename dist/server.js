c52f3fca6da251e5e4a9265bf4c818da7fd37dd7
NeptuneRjo <rjo9000@outlook.com>
2022-08-06 19:19:19 -0400

    compile

--- dist/server.js
+++ dist/server.js
@@ -5,12 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = require("mongoose");
+const middleware_exports_1 = require("./middleware/middleware-exports");
+const passport_1 = __importDefault(require("passport"));
 require("dotenv/config");
 const routes_exports_1 = require("./routes/routes-exports");
 const app = (0, express_1.default)();
 // <-- Middleware -->
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
+// passport
+passport_1.default.use(middleware_exports_1.passportLocal);
 // <-- Routes -->
 app.get('/', (req, res) => res.redirect('/api/users'));
 app.use('/api/users', routes_exports_1.userRoutes);
