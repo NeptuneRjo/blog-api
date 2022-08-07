495b105e4ac53baa15e4d5f9ce1e231f53482a35
NeptuneRjo <rjo9000@outlook.com>
2022-08-06 20:33:49 -0400

    add route for login and impliment login controller, signup controller is now for the /signup route

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import {
-	post_user,
+	signup_user,
 	delete_user,
 	get_user,
+	login_user,
 } from '../controllers/userControllers'
 const router = Router()
 
-router.route('/').post(post_user)
+router.route('/signup').post(signup_user)
+router.route('/login').post(login_user)
 router.route('/:id').get(get_user).delete(delete_user)
 
 export default router
