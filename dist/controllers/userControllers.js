ff9f5894affa3ed49e8daa6cf2e1dc97dbe98d0e
NeptuneRjo <rjo9000@outlook.com>
2022-08-06 20:33:59 -0400

    compile

--- dist/controllers/userControllers.js
+++ dist/controllers/userControllers.js
@@ -8,8 +8,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.delete_user = exports.post_user = exports.get_user = void 0;
+exports.delete_user = exports.login_user = exports.signup_user = exports.get_user = void 0;
+const passport_1 = __importDefault(require("passport"));
 const models_exports_1 = require("../models/models-exports");
 // GET Requests
 const get_user = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -29,7 +33,7 @@ const get_user = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
 });
 exports.get_user = get_user;
 // POST Requests
-const post_user = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const signup_user = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const user = yield models_exports_1.User.create(req.body);
         res.status(200).json({ message: 'User successfully created' });
@@ -38,7 +42,12 @@ const post_user = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         res.status(400).json({ error });
     }
 });
-exports.post_user = post_user;
+exports.signup_user = signup_user;
+const login_user = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    passport_1.default.authenticate('local');
+    res.status(201).json({ user: req === null || req === void 0 ? void 0 : req.user });
+});
+exports.login_user = login_user;
 // DELETE Requests
 const delete_user = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
--- dist/routes/userRoutes.js
+++ dist/routes/userRoutes.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const userControllers_1 = require("../controllers/userControllers");
 const router = (0, express_1.Router)();
-router.route('/').post(userControllers_1.post_user);
+router.route('/signup').post(userControllers_1.signup_user);
+router.route('/login').post(userControllers_1.login_user);
 router.route('/:id').get(userControllers_1.get_user).delete(userControllers_1.delete_user);
 exports.default = router;
