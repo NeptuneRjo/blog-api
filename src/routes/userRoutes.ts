d13fd1d47c6919f0249357ada7f7d12ce7bc01c9
NeptuneRjo <rjo9000@outlook.com>
2022-08-08 02:39:39 -0400

    add login controller, update routes to use signup and login

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,18 +1,15 @@
 import { Router } from 'express'
 import passport from 'passport'
 import {
-	post_user,
+	signup_user,
 	delete_user,
 	get_user,
+	login_user,
 } from '../controllers/userControllers'
 const router = Router()
 
-router.route('/').post(post_user)
-router
-	.route('/login')
-	.post(passport.authenticate('local'), (req, res, next) => {
-		res.status(200).json({ msg: 'authenticated', user: req.user })
-	})
+router.route('/signup').post(signup_user)
+router.route('/login').post(passport.authenticate('local'), login_user)
 router.route('/:id').get(get_user).delete(delete_user)
 
 export default router
