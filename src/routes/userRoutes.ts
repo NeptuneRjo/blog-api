8bd406fcd6395dc025598eef1acb874632a11f98
NeptuneRjo <rjo9000@outlook.com>
2022-08-07 20:33:26 -0400

    add passport authenticate middleware and return authenticated user on login

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import passport from 'passport'
 import {
 	post_user,
 	delete_user,
@@ -7,6 +8,11 @@ import {
 const router = Router()
 
 router.route('/').post(post_user)
+router
+	.route('/login')
+	.post(passport.authenticate('local'), (req, res, next) => {
+		res.status(200).json({ msg: 'authenticated', user: req.user })
+	})
 router.route('/:id').get(get_user).delete(delete_user)
 
 export default router
