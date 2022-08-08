fa721dbf746dd1374048400a1a16f4077e964c71
NeptuneRjo <rjo9000@outlook.com>
2022-08-08 02:13:37 -0400

    add passport strategy and serialization to middleware module that can be used in the server

--- src/middleware/passportMiddleware.ts
+++ src/middleware/passportMiddleware.ts
@@ -1,27 +1,40 @@
 import { Strategy as LocalStrategy } from 'passport-local'
 import { User } from '../models/models-exports'
 import { compare } from 'bcryptjs'
+import passport from 'passport'
 
 const message = 'Incorrect password'
 
-export const passportLocal = new LocalStrategy(
-	{ usernameField: 'email', passwordField: 'password' },
-	(email, password, done) => {
-		console.log('local')
-		User.findOne({ email }, (err: any, user: { password: string }) => {
-			if (err) return done(err)
-			if (!user) return done(null, false)
+export function passportLocal() {
+	passport.use(
+		new LocalStrategy(
+			{ usernameField: 'email', passwordField: 'password' },
+			(email, password, done) => {
+				console.log('local')
+				User.findOne({ email }, (err: any, user: { password: string }) => {
+					if (err) return done(err)
+					if (!user) return done(null, false)
 
-			if (password !== user.password) {
-				compare(password, user.password, (err, res) => {
-					if (res) {
-						console.log(res)
-						return done(null, user)
-					} else {
-						return done(null, false, { message: message })
+					if (password !== user.password) {
+						compare(password, user.password, (err, res) => {
+							if (res) {
+								console.log(res)
+								return done(null, user)
+							} else {
+								return done(null, false, { message: message })
+							}
+						})
 					}
 				})
 			}
+		)
+	)
+
+	passport.serializeUser((id, done) => done(null, id))
+
+	passport.deserializeUser((id, done) => {
+		User.findById(id, (err: any, user: any) => {
+			done(err, user)
 		})
-	}
-)
+	})
+}
