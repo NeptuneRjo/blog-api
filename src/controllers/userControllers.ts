8fe8362edae5c7ad06838d2a3e0817448358e3ab
NeptuneRjo <rjo9000@outlook.com>
2022-08-08 02:37:29 -0400

    add login controller that returns the users email and id

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -19,12 +19,31 @@ export const get_user = async (req: Request, res: Response): Promise<void> => {
 }
 
 // POST Requests
-export const post_user = async (req: Request, res: Response): Promise<void> => {
+export const signup_user = async (
+	req: Request,
+	res: Response
+): Promise<void> => {
 	try {
 		const user = await User.create(req.body)
 		res.status(200).json({ message: 'User successfully created' })
 	} catch (error) {
 		res.status(400).json({ error })
+	}
+}
+
+export const logout_user = async (
+	req: Request,
+	res: Response
+): Promise<void> => {
+	let user = req.user as any
+
+	if (req.user) {
+		res.status(200).json({
+			msg: 'User authenticated',
+			user: { email: user.email, id: user._id },
+		})
+	} else {
+		res.status(400).json({ msg: 'Unable to authenticate user' })
 	}
 }
 
