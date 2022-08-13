0f9a60ae7303184eed8b0bed40e59ef00a724697
NeptuneRjo <rjo9000@outlook.com>
2022-08-13 02:47:07 -0400

    add logout route

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,12 +6,15 @@ import {
 	get_user,
 	login_user,
 	logout_user,
+	get_current_user,
 } from '../controllers/userControllers'
 const router = Router()
 
 router.post('/signup', signup_user)
 router.post('/login', passport.authenticate('local'), login_user)
 router.post('/logout', logout_user)
+
+router.get('/', get_current_user)
 
 router.route('/:id').get(get_user).delete(delete_user)
 
