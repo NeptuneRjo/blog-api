46588551577fcba4f5a0605e503954611b44752b
NeptuneRjo <rjo9000@outlook.com>
2022-08-16 18:19:50 -0400

    add user auth middleware to post and delete

--- src/routes/blogRoutes.ts
+++ src/routes/blogRoutes.ts
@@ -6,10 +6,15 @@ import {
 	delete_blog,
 	patch_blog,
 } from '../controllers/blogControllers'
+import { checkIfUserAuthed } from '../middleware/middleware-exports'
 
 const router = Router()
 
-router.route('/').get(get_all_blogs).post(post_blog)
-router.route('/:id').get(get_blog).patch(patch_blog).delete(delete_blog)
+router.route('/').get(get_all_blogs).post(checkIfUserAuthed, post_blog)
+router
+	.route('/:id')
+	.get(get_blog)
+	.patch(patch_blog)
+	.delete(checkIfUserAuthed, delete_blog)
 
 export default router
