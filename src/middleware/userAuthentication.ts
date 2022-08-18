5c805fc042cfcfaf8df045c865c7bbaf27bd1523
NeptuneRjo <rjo9000@outlook.com>
2022-08-18 16:44:30 -0400

    add switch case for different auth levels

--- src/middleware/userAuthentication.ts
+++ src/middleware/userAuthentication.ts
@@ -5,10 +5,22 @@ const checkIfUserAuthed = (
 	res: Response,
 	next: NextFunction
 ): void | NextFunction => {
-	if (req.user) {
-		next()
-	} else {
-		res.status(401).json({ error: 'Must log in before accessing this content' })
+	const user = req.user as any
+	switch (req.user) {
+		case undefined:
+			res
+				.status(401)
+				.json({ error: 'Must log in before accessing this content' })
+		case user.role === 'Admin':
+			next()
+		case user.role === 'User':
+			res
+				.status(403)
+				.json({ error: 'You are not authorized to access this content' })
+		default:
+			res
+				.status(401)
+				.json({ error: 'Must log in before accessing this content' })
 	}
 }
 
