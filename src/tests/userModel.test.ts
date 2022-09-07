da3779e74813c2ea581b48132ab951c49b7ed220
NeptuneRjo <rjo9000@outlook.com>
2022-09-07 03:43:08 -0400

    create user model test cases

---
+++ src/tests/userModel.test.ts
@@ -0,0 +1,40 @@
+import User from '../models/userModel'
+import { fakeUserData, fakeFailUserData } from './fixtures'
+import 'jest'
+import {
+	initializeMongoServer,
+	deinitializeMongoServer,
+	dropCollections,
+} from '../config/mongoConfigTesting'
+
+describe('User Model', () => {
+	beforeAll(async () => {
+		const mongoServer = await initializeMongoServer()
+	})
+
+	afterAll(async () => {
+		await deinitializeMongoServer()
+	})
+
+	afterEach(async () => {
+		await dropCollections()
+	})
+
+	it('should create a user item successfully', async () => {
+		const newBlog = await User.create(fakeUserData)
+
+		expect(newBlog._id).toBeDefined()
+		expect(newBlog.email).toBe(newBlog.email)
+		expect(newBlog.role).toBe(newBlog.role)
+		expect(newBlog.username).toBe(newBlog.username)
+		expect(newBlog.password).toBe(newBlog.password)
+	})
+
+	it('should fail the user item without the required fields', async () => {
+		const newBlog = new User(fakeFailUserData)
+
+		newBlog.validate((err) => {
+			expect(err).not.toBe(null)
+		})
+	})
+})
