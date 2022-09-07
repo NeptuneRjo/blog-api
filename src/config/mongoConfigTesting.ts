c62e2d88dc39f808cd6d896f364a28ee55f1ae3f
NeptuneRjo <rjo9000@outlook.com>
2022-09-07 03:13:31 -0400

    add deinitialize and collection drop functions

--- src/config/mongoConfigTesting.ts
+++ src/config/mongoConfigTesting.ts
@@ -1,8 +1,10 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import { connect, connection } from 'mongoose'
 
-const initializeMongoServer = async () => {
-	const mongoServer = await MongoMemoryServer.create()
+let mongoServer: null | MongoMemoryServer = null
+
+export const initializeMongoServer = async () => {
+	mongoServer = await MongoMemoryServer.create()
 	const mongoUri = mongoServer.getUri()
 
 	connect(mongoUri)
@@ -16,4 +18,20 @@ const initializeMongoServer = async () => {
 	})
 }
 
-export default initializeMongoServer
+export const deinitializeMongoServer = async () => {
+	if (mongoServer) {
+		await connection.dropDatabase()
+		await connection.close()
+		await mongoServer.stop()
+	}
+}
+
+export const dropCollections = async () => {
+	if (mongoServer) {
+		const collections = await connection.db.collections()
+
+		for (let collection of collections) {
+			await collection.drop()
+		}
+	}
+}
