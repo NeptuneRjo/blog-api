83eca92d174fbaa148eca68d11b72d880c6a183e
NeptuneRjo <rjo9000@outlook.com>
2022-09-07 03:23:40 -0400

    add mongo testing config and basic testing of mongo model

---
+++ src/tests/blogModel.test.ts
@@ -0,0 +1,41 @@
+import Blog from '../models/blogModel'
+import { fakeBlogData, fakeFailBlogData } from './fixtures'
+import 'jest'
+import {
+	initializeMongoServer,
+	deinitializeMongoServer,
+	dropCollections,
+} from '../config/mongoConfigTesting'
+
+describe('Blog Model', () => {
+	beforeAll(async () => {
+		const mongoServer = await initializeMongoServer()
+	})
+
+	afterAll(async () => {
+		await deinitializeMongoServer()
+	})
+
+	afterEach(async () => {
+		await dropCollections()
+	})
+
+	it('should create a blog item successfully', async () => {
+		const newBlog = await Blog.create(fakeBlogData)
+
+		expect(newBlog._id).toBeDefined()
+		expect(newBlog.body).toBe(newBlog.body)
+		expect(newBlog.title).toBe(newBlog.title)
+		expect(newBlog.date).toBe(newBlog.date)
+		expect(newBlog.comments).toBe(newBlog.comments)
+	})
+
+	it('should fail the blog item without the required fields', async () => {
+		const newBlog = new Blog(fakeFailBlogData)
+
+		newBlog.validate((err) => {
+			// console.log(err)
+			expect(err).not.toBe(null)
+		})
+	})
+})
