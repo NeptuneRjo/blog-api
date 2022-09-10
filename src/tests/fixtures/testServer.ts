fa49fbaf79da9bf342be4d8e2080746f3685aaa5
NeptuneRjo <rjo9000@outlook.com>
2022-09-10 03:13:51 -0400

    build testServer application to use when running tests

---
+++ src/tests/fixtures/testServer.ts
@@ -0,0 +1,69 @@
+import express from 'express'
+import session from 'express-session'
+import { connect, connection } from 'mongoose'
+import { passportLocal } from '../../middleware/middleware-exports'
+import passport from 'passport'
+import cors from 'cors'
+// import './config/mongoConfig'
+import 'dotenv/config'
+import { initializeMongoServer } from '../../config/mongoConfigTesting'
+
+import { blogRoutes, userRoutes } from '../../routes/routes-exports'
+
+export const app = express()
+
+app.use(
+	cors({
+		origin: [
+			'https://neptunerjo.github.io',
+			'https://neptunerjo.github.io/',
+			'http://localhost:3000',
+		],
+		methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'DELETE', 'PATCH'],
+		credentials: true,
+	})
+)
+
+passportLocal() // Passport strategy and serialization
+
+// <-- Middleware -->
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(
+	session({
+		secret: process.env.WEB_SECRET as string,
+		resave: true,
+		saveUninitialized: false,
+		rolling: true,
+		cookie: {
+			sameSite: 'none',
+		},
+	})
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+// <-- Routes -->
+app.get('/', (req, res) => res.send('Hello World'))
+app.use('/api/users', userRoutes)
+app.use('/api/blogs', blogRoutes)
+
+// <-- DB & App start -->
+const port = process.env.PORT || 4000
+
+// connection.on('connected', () => {
+// 	app.listen(port, () =>
+// 		console.log('Connected to DB and listening on port:', port)
+// 	)
+// })
+
+initializeMongoServer()
+
+connection.on('connected', () => {
+	app.listen(port, () =>
+		console.log('Connected to DB and listening on port:', port)
+	)
+})
