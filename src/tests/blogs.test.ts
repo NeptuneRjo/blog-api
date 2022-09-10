00846600d8d2f18984115d1cad9e89f08e21ff17
NeptuneRjo <rjo9000@outlook.com>
2022-09-10 03:58:58 -0400

    created tests for blog-related API functionality

--- src/tests/blogs.test.ts
+++ src/tests/blogs.test.ts
@@ -11,7 +11,8 @@ import 'dotenv/config'
 const server = request.agent('http://localhost:4000')
 
 describe('Blog Tests', () => {
-	let token: any
+	let token: string
+	let id: string
 
 	beforeAll(async () => {
 		const mongoServer = await initializeMongoServer()
@@ -93,5 +94,107 @@ describe('Blog Tests', () => {
 		})
 	})
 
-	describe.skip('DELETE /api/blogs/:id', () => {})
+	describe('DELETE /api/blogs/:id', () => {
+		createUser()
+		loginUser()
+
+		// Creates the blog to be deleted
+		it('POST new blog', (done) => {
+			server
+				.post('/api/blogs')
+				.send(newBlog)
+				.query(token)
+				.expect(201)
+				.end((err, res) => {
+					id = res.body.data._id
+					if (err) return done(err)
+					return done()
+				})
+		})
+
+		it('DELETES blog', (done) => {
+			server
+				.delete(`/api/blogs/${id}`)
+				.query(token)
+				.expect(200)
+				.end((err, res) => {
+					if (err) return done(err)
+					return done()
+				})
+		})
+	})
+
+	describe('GET /api/blogs', () => {
+		it('GETS all blogs', (done) => {
+			server
+				.get('/api/blogs')
+				.expect(200)
+				.expect('Content-Type', /json/)
+				.end((err, res) => {
+					if (err) return done(err)
+					return done()
+				})
+		})
+	})
+
+	describe('GET /api/blogs/:id', () => {
+		createUser()
+		loginUser()
+
+		// Creates the blog to be found
+		it('POST new blog', (done) => {
+			server
+				.post('/api/blogs')
+				.send(newBlog)
+				.query(token)
+				.expect(201)
+				.end((err, res) => {
+					id = res.body.data._id
+					if (err) return done(err)
+					return done()
+				})
+		})
+
+		it('GETS the blogs', (done) => {
+			server
+				.get(`/api/blogs/${id}`)
+				.expect(200)
+				.expect('Content-Type', /json/)
+				.end((err, res) => {
+					if (err) return done(err)
+					return done()
+				})
+		})
+	})
+
+	describe('PATCH /api/blogs/:id', () => {
+		createUser()
+		loginUser()
+
+		// Creates the blog to be patched
+		it('POST new blog', (done) => {
+			server
+				.post('/api/blogs')
+				.send(newBlog)
+				.query(token)
+				.expect(201)
+				.end((err, res) => {
+					id = res.body.data._id
+					if (err) return done(err)
+					return done()
+				})
+		})
+
+		it('PATCHes the blogs', (done) => {
+			server
+				.patch(`/api/blogs/${id}`)
+				.send({ comments: [{ username: 'test', body: 'test' }] })
+				.expect(200)
+				.expect('Content-Type', /json/)
+				.end((err, res) => {
+					if (err) return done(err)
+					return done()
+				})
+		})
+	})
 })
